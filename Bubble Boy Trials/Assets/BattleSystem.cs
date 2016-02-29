bf928d7a7189f127468c848671739a8dd72e0692
Max Dietz <mdietz@wesleyan.edu>
2016-02-29 14:12:28 -0500

    removed unused files in .gitignore

--- Bubble Boy Trials/Assets/BattleSystem.cs
+++ Bubble Boy Trials/Assets/BattleSystem.cs
@@ -42,32 +42,34 @@ public class BattleSystem : MonoBehaviour {
         timeRemaining -= Time.fixedDeltaTime;
 	    switch (current_state) {
             case BattleState.player_turn:
-                time_left.text = timeRemaining.ToString();
+                time_left.text = Mathf.CeilToInt(timeRemaining).ToString();
                 if (timeRemaining < 0)
                 {
+                    Debug.Log("TRANSITION");
                     EnemyTurn();
                 }
                 break;
             case BattleState.enemy_turn:
-                time_left.text = "WAITING...";
+                if (timeRemaining < ReactionWindow)
+                {
+                    time_left.text = "NOW!";
+                } else {
+                    time_left.text = "WAITING...";
+                }
                 if (timeRemaining < 0)
                 {
                     PlayerTurn();
                     player.SendMessage("TakeDamage", 10);
 
                 }
-                else if (timeRemaining < ReactionWindow && Input.GetKeyDown(KeyCode.Space))
+                else if (timeRemaining < ReactionWindow && Input.GetKeyDown(KeyCode.X))
                 {
                     PlayerTurn();
                 }
-                else if (Input.GetKeyDown(KeyCode.Space))
+                else if (Input.GetKeyDown(KeyCode.X))
                 {
+                    PlayerTurn();
                     player.SendMessage("TakeDamage", 20);
-                    PlayerTurn();
-                }
-                if (timeRemaining < ReactionWindow)
-                {
-                    time_left.text = "NOW!";
                 }
                 break;
         }
@@ -75,29 +77,38 @@ public class BattleSystem : MonoBehaviour {
 
     private void PlayerTurn()
     {
+        timeRemaining = TimePerTurn;
         current_state = BattleState.player_turn;
+        answer1.onClick.RemoveAllListeners();
+        answer2.onClick.RemoveAllListeners();
+        answer3.onClick.RemoveAllListeners();
+        answer4.onClick.RemoveAllListeners();
         answer1.gameObject.SetActive(true);
         answer2.gameObject.SetActive(true);
         answer3.gameObject.SetActive(true);
         answer4.gameObject.SetActive(true);
-        timeRemaining = TimePerTurn;
         variable_one = Mathf.CeilToInt(Random.Range(0, 10));
         variable_two = Mathf.CeilToInt(Random.Range(0, 10));
         problem.text = variable_one.ToString() + " + " + variable_two.ToString() + " = ?";
+        problem.gameObject.SetActive(true);
         for (int i = 0; i < 4; ++i)
         {
-            answers[i] = Mathf.CeilToInt(Random.Range(0, 20));
+            bool unique = false;
+            // we can't have two bubbles with the same answer
+            while (!unique) {
+                answers[i] = Mathf.CeilToInt(Random.Range(0, 20));
+                unique = answers[i] != variable_one + variable_two;
+                for (int x = 0; x < i; ++x) {
+                    unique = unique && answers[i] != answers[x];
+                }
+            }
         }
         int right_index = Mathf.FloorToInt(Random.Range(0, 3.999f));
         answers[right_index] = variable_one + variable_two;
         answer1.GetComponentInChildren<Text>().text = answers[0].ToString();
-        answer1.onClick.RemoveAllListeners();
         answer2.GetComponentInChildren<Text>().text = answers[1].ToString();
-        answer2.onClick.RemoveAllListeners();
         answer3.GetComponentInChildren<Text>().text = answers[2].ToString();
-        answer3.onClick.RemoveAllListeners();
         answer4.GetComponentInChildren<Text>().text = answers[3].ToString();
-        answer4.onClick.RemoveAllListeners();
         // there's definitely a better way to do this... but for now...
         switch (right_index)
         {
@@ -145,6 +156,7 @@ public class BattleSystem : MonoBehaviour {
         answer2.gameObject.SetActive(false);
         answer3.gameObject.SetActive(false);
         answer4.gameObject.SetActive(false);
+        problem.gameObject.SetActive(false);
         current_state = BattleState.enemy_turn;
         timeRemaining = Random.Range(2, 2 + TimePerTurn);
     }
--- Bubble Boy Trials/Assets/Player.cs
+++ Bubble Boy Trials/Assets/Player.cs
@@ -5,22 +5,24 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
     public int Health = 100;
-
+    public GameObject HealthPrefab;
+    private GameObject healthtxt;
+    
 	// Use this for initialization
 	void Start () {
-        GetComponent<GUIText>().text = Health.ToString();
+            healthtxt = GameObject.Instantiate(HealthPrefab, new Vector3(0,0,0), Quaternion.identity) as GameObject;
+            healthtxt.GetComponent<GUIText>().text = Health.ToString();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        GUIText text = GetComponent<GUIText>();
-        text.pixelOffset = Camera.main.WorldToScreenPoint(transform.position);
+            healthtxt.GetComponent<GUIText>().pixelOffset = Camera.main.WorldToScreenPoint(transform.position);
     }
 
     void TakeDamage(int damage)
     {
         Health -= damage;
-        GetComponent<GUIText>().text = Health.ToString();
+        healthtxt.GetComponent<GUIText>().text = Health.ToString();
         if (Health < 0)
         {
             Destroy(this);
