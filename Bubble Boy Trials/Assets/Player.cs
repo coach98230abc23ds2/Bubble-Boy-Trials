734f6a2347de62d39ff0f3175f3466987a098291
Max Dietz <mdietz@wesleyan.edu>
2016-03-01 14:07:33 -0500

    sprites, health bars

--- Bubble Boy Trials/Assets/Player.cs
+++ Bubble Boy Trials/Assets/Player.cs
@@ -4,28 +4,46 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-    public int Health = 100;
+    public int Health = 30;
     public GameObject HealthPrefab;
-    private GameObject healthtxt;
+    public Sprite IdleSprite;
+    public Sprite HurtSprite;
+    public Sprite DeadSprite;
+    private float animCycle;
+    private GameObject healthBar;
     
 	// Use this for initialization
 	void Start () {
-            healthtxt = GameObject.Instantiate(HealthPrefab, new Vector3(0,0,0), Quaternion.identity) as GameObject;
-            healthtxt.GetComponent<GUIText>().text = Health.ToString();
+            healthBar = GameObject.Instantiate(HealthPrefab, new Vector3(0,0,0), Quaternion.identity) as GameObject;
+            healthBar.transform.SetParent(GameObject.Find("Canvas").GetComponent<RectTransform>(), false);
+            healthBar.GetComponent<Slider>().value = Health;
+            GetComponent<SpriteRenderer>().sprite = IdleSprite;
     }
 	
 	// Update is called once per frame
 	void Update () {
-            healthtxt.GetComponent<GUIText>().pixelOffset = Camera.main.WorldToScreenPoint(transform.position);
+            RectTransform CanvasRect = GameObject.Find("Canvas").GetComponent<RectTransform>();
+            Vector2 ViewportPos = Camera.main.WorldToViewportPoint(transform.position);
+            Vector2 ScreenPos = new Vector2((ViewportPos.x*CanvasRect.sizeDelta.x)-(CanvasRect.sizeDelta.x*0.5f),(ViewportPos.y*CanvasRect.sizeDelta.y)-(CanvasRect.sizeDelta.y*0.5f));
+            healthBar.GetComponent<RectTransform>().anchoredPosition = ScreenPos + new Vector2(0,50);
+            animCycle -= Time.deltaTime;
+            if (animCycle < 0) {
+                animCycle = 0.5f;
+            if (GetComponent<SpriteRenderer>().sprite == HurtSprite)
+                GetComponent<SpriteRenderer>().sprite = IdleSprite;
+            else
+                GetComponent<SpriteRenderer>().sprite = HurtSprite;
+            }
     }
 
     void TakeDamage(int damage)
     {
         Health -= damage;
-        healthtxt.GetComponent<GUIText>().text = Health.ToString();
-        if (Health < 0)
+        healthBar.GetComponent<Slider>().value = Health;
+        if (Health <= 0)
         {
-            Destroy(this);
+            gameObject.SetActive(false);
         }
+        GetComponent<SpriteRenderer>().sprite = DeadSprite;
     }
 }
