5047f201daf26656e5f59361b05c72de3ba194a0
Max Dietz <mdietz@wesleyan.edu>
2016-03-19 18:00:35 -0400

    added assertions, and cleaned up code

--- Bubble Boy Trials/Assets/BattleSystem.cs
+++ Bubble Boy Trials/Assets/BattleSystem.cs
@@ -1,166 +1,161 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
+using UnityEngine.Assertions;
+using System.Collections.Generic;
 
-public class BattleSystem : MonoBehaviour {
+public class BattleSystem : MonoBehaviour
+{
 
-    public enum BattleState {
-        player_turn,
-        enemy_turn
-    };
+		public enum BattleState
+		{
+				player_turn,
+				enemy_turn}
 
-    private GameObject player;
-    public GameObject enemy;
-    private BattleState current_state;
+		;
 
-    [Range(1, 30)] public float TimePerTurn = 10000;
-    [Range(0.25f,2)] public float ReactionWindow = 1000;
+		private GameObject player;
+		public GameObject enemy;
+		private BattleState current_state;
 
-    private float timeRemaining;
-    private float timeToDefend;
+		[Range (1, 30)] public float TimePerTurn = 10000;
+		[Range (0.25f, 2)] public float ReactionWindow = 1000;
 
-    private int variable_one;
-    private int variable_two;
-    private int[] answers = new int[4];
+		private float timeRemaining;
+		private float timeToDefend;
 
-    public Button answer1;
-    public Button answer2;
-    public Button answer3;
-    public Button answer4;
+		private int variable_one;
+		private int variable_two;
+		private List<Button> answers;
 
-    public Slider time_remaining;
-    public Text time_left;
-    public Text problem;
 
-    // Use this for initialization
-    void Start () {
-        player = GameObject.FindGameObjectWithTag("Player");
-        PlayerTurn();
-    }
+		public Button answer1;
+		public Button answer2;
+		public Button answer3;
+		public Button answer4;
+
+		public Slider time_remaining;
+		public Text time_left;
+		public Text problem;
+
+		// Use this for initialization
+		void Start ()
+		{
+				player = GameObject.FindGameObjectWithTag ("Player");
+				answers = new List<Button> { answer1, answer2, answer3, answer4 };
+				PlayerTurn ();
+		}
 	
-	// Update is called once per frame
-	void FixedUpdate () {
-        timeRemaining -= Time.fixedDeltaTime;
-	    switch (current_state) {
-            case BattleState.player_turn:
-                time_remaining.value = timeRemaining;
-                if (timeRemaining < 0)
-                {
-                    Debug.Log("TRANSITION");
-                    EnemyTurn();
-                }
-                break;
-            case BattleState.enemy_turn:
-                if (timeRemaining < ReactionWindow)
-                {
-                    time_left.text = "NOW!";
-                } else {
-                    time_left.text = "WAITING...";
-                }
-                if (timeRemaining < 0)
-                {
-                    PlayerTurn();
-                    player.SendMessage("TakeDamage", 10);
+		// Update is called once per frame
+		void FixedUpdate ()
+		{
+				timeRemaining -= Time.fixedDeltaTime;
+				switch (current_state) {
+				case BattleState.player_turn:
+						time_remaining.value = timeRemaining;
+						if (timeRemaining < 0) {
+								EnemyTurn ();
+						}
+						break;
+				case BattleState.enemy_turn:
+						if (timeRemaining < ReactionWindow) {
+								time_left.text = "NOW!";
+						} else {
+								time_left.text = "WAITING...";
+						}
+						if (timeRemaining < 0) {
+								PlayerTurn ();
+								player.SendMessage ("TakeDamage", 10);
 
-                }
-                else if (timeRemaining < ReactionWindow && Input.GetKeyDown(KeyCode.X))
-                {
-                    PlayerTurn();
-                }
-                else if (Input.GetKeyDown(KeyCode.X))
-                {
-                    PlayerTurn();
-                    player.SendMessage("TakeDamage", 20);
-                }
-                break;
-        }
-	}
+						} else if (timeRemaining < ReactionWindow && Input.GetKeyDown (KeyCode.X)) {
+								PlayerTurn ();
+						} else if (Input.GetKeyDown (KeyCode.X)) {
+								PlayerTurn ();
+								player.SendMessage ("TakeDamage", 20);
+						}
+						break;
+				}
+		}
 
-    private void PlayerTurn()
-    {
-        timeRemaining = TimePerTurn;
-        current_state = BattleState.player_turn;
-        answer1.onClick.RemoveAllListeners();
-        answer2.onClick.RemoveAllListeners();
-        answer3.onClick.RemoveAllListeners();
-        answer4.onClick.RemoveAllListeners();
-        answer1.gameObject.SetActive(true);
-        answer2.gameObject.SetActive(true);
-        answer3.gameObject.SetActive(true);
-        answer4.gameObject.SetActive(true);
-        time_remaining.gameObject.SetActive(true);
-        variable_one = Mathf.CeilToInt(Random.Range(0, 10));
-        variable_two = Mathf.CeilToInt(Random.Range(0, 10));
-        problem.text = variable_one.ToString() + " + " + variable_two.ToString() + " = ?";
-        problem.gameObject.SetActive(true);
-        for (int i = 0; i < 4; ++i)
-        {
-            bool unique = false;
-            // we can't have two bubbles with the same answer
-            while (!unique) {
-                answers[i] = Mathf.CeilToInt(Random.Range(0, 20));
-                unique = answers[i] != variable_one + variable_two;
-                for (int x = 0; x < i; ++x) {
-                    unique = unique && answers[i] != answers[x];
-                }
-            }
-        }
-        int right_index = Mathf.FloorToInt(Random.Range(0, 3.999f));
-        answers[right_index] = variable_one + variable_two;
-        answer1.GetComponentInChildren<Text>().text = answers[0].ToString();
-        answer2.GetComponentInChildren<Text>().text = answers[1].ToString();
-        answer3.GetComponentInChildren<Text>().text = answers[2].ToString();
-        answer4.GetComponentInChildren<Text>().text = answers[3].ToString();
-        // there's definitely a better way to do this... but for now...
-        switch (right_index)
-        {
-            case 0:
-                answer1.onClick.AddListener(RightAnswer);
-                answer2.onClick.AddListener(WrongAnswer);
-                answer3.onClick.AddListener(WrongAnswer);
-                answer4.onClick.AddListener(WrongAnswer);
-                break;
-            case 1:
-                answer2.onClick.AddListener(RightAnswer);
-                answer1.onClick.AddListener(WrongAnswer);
-                answer3.onClick.AddListener(WrongAnswer);
-                answer4.onClick.AddListener(WrongAnswer);
-                break;
-            case 2:
-                answer3.onClick.AddListener(RightAnswer);
-                answer1.onClick.AddListener(WrongAnswer);
-                answer2.onClick.AddListener(WrongAnswer);
-                answer4.onClick.AddListener(WrongAnswer);
-                break;
-            default:
-                answer4.onClick.AddListener(RightAnswer);
-                answer1.onClick.AddListener(WrongAnswer);
-                answer2.onClick.AddListener(WrongAnswer);
-                answer3.onClick.AddListener(WrongAnswer);
-                break;
-        }
-    }
 
-    private void RightAnswer()
-    {
-        enemy.SendMessage("TakeDamage", 10);
-        EnemyTurn();
-    }
+		private void ResetButton (Button button)
+		{
+				button.gameObject.SetActive (true);
+				SetListener (button, false);
+		}
 
-    private void WrongAnswer()
-    {
-        EnemyTurn();
-    }
 
-    private void EnemyTurn()
-    {
-        answer1.gameObject.SetActive(false);
-        answer2.gameObject.SetActive(false);
-        answer3.gameObject.SetActive(false);
-        answer4.gameObject.SetActive(false);
-        problem.gameObject.SetActive(false);
-        time_remaining.gameObject.SetActive(false);
-        current_state = BattleState.enemy_turn;
-        timeRemaining = Random.Range(2, 2 + TimePerTurn);
-    }
+		/* Sets the button's only listener to be Right/WrongAnswer */
+		private void SetListener (Button button, bool isRightAnswer)
+		{
+				button.onClick.RemoveAllListeners ();
+				if (isRightAnswer) {
+						button.onClick.AddListener (RightAnswer);
+				} else {
+						button.onClick.AddListener (WrongAnswer);
+				}
+		}
+
+		private void ShowPlayerUI ()
+		{
+				answers.ForEach (b => ResetButton (b));
+				problem.gameObject.SetActive (true);
+				time_remaining.gameObject.SetActive (true);
+		}
+
+		private void HidePlayerUI ()
+		{
+				answers.ForEach (b => b.gameObject.SetActive (false));
+				problem.gameObject.SetActive (false);
+				time_remaining.gameObject.SetActive (false);
+		}
+
+
+		private void PlayerTurn ()
+		{
+				timeRemaining = TimePerTurn;
+				current_state = BattleState.player_turn;
+
+				// reset buttons and UI elements
+				ShowPlayerUI ();
+
+				// generate new problem
+				Problem newProblem = new Problem ();
+		
+				// set question text fields based on new problem
+				problem.text = newProblem.ToString();
+				problem.gameObject.SetActive (true);
+		
+				// set solution text fields based on new Problem
+				int[] possibleSolutions = newProblem.GetPossibleSolutions ();
+
+				// although this is set as a variable in newProblem
+				// we need to make UI buttons by hand on the scene, so
+				// we should ensure it is equal to 4
+				Assert.AreEqual (possibleSolutions.GetLength (0), answers.Count);
+				for (int i = 0; i < answers.Count; ++i) {
+						answers [i].GetComponentInChildren<Text> ().text = possibleSolutions [i].ToString ();
+				}
+
+				SetListener (answers [newProblem.GetSolutionIndex ()], true);
+		}
+
+		private void RightAnswer ()
+		{
+				enemy.SendMessage ("TakeDamage", 10);
+
+				HidePlayerUI ();
+				EnemyTurn ();
+		}
+
+		private void WrongAnswer ()
+		{
+				HidePlayerUI ();
+				EnemyTurn ();
+		}
+
+		private void EnemyTurn ()
+		{
+				current_state = BattleState.enemy_turn;
+				timeRemaining = Random.Range (2, 2 + TimePerTurn);
+		}
 }
---
+++ Bubble Boy Trials/Assets/Problem.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections.Generic;
+
+/*
+
+  This class represents a problem that will be generated for each turn of combat.
+  It currently supports addition, multiplication, and subtraction questions,
+  and is responsible for creating a list of possible solutions and storing
+  the actual solution.
+
+ */
+
+public class Problem
+{
+
+		public enum Operator
+		{
+				PLUS,
+				MINUS,
+				TIMES
+		};
+
+		static int SOLUTION_COUNT = 4;
+		static int MAX_OPERAND = 10;
+
+		int m_operandFirst;
+		int m_operandSecond;
+		Operator m_operator;
+		List<int> m_solutionList;
+
+		public Problem (Operator problemType = Operator.PLUS)
+		{
+				m_operandFirst = Mathf.CeilToInt (Random.Range (0, 10));
+				m_operandSecond = Mathf.CeilToInt (Random.Range (0, 10));
+				m_operator = problemType;
+
+				// now ensure that we don't end up giving negative solutions
+				if (m_operator == Operator.MINUS && m_operandFirst < m_operandSecond) {
+						int swap = m_operandFirst;
+						m_operandFirst = m_operandSecond;
+						m_operandSecond = swap;
+				}
+        
+				m_solutionList = new List<int> (SOLUTION_COUNT);
+        
+				// fill solutionList with dummy solutions
+				// making sure every value is unique
+				for (int i = 0; i < SOLUTION_COUNT; ++i) {
+						int possibleSolution;
+						do {
+								possibleSolution = Mathf.RoundToInt (Random.Range (0, MaxSolution ()));
+						} while (m_solutionList.Contains (possibleSolution) || possibleSolution == GetSolution ());
+                
+						m_solutionList.Add (possibleSolution);
+				}
+
+				// now fill in a random position with the actual solution
+				int rightIndex = Mathf.RoundToInt (Random.Range (0, SOLUTION_COUNT - 1));
+				m_solutionList [rightIndex] = GetSolution ();
+
+				// ensure our list is made of unique elements
+				Assert.IsTrue (m_solutionList.TrueForAll (s => m_solutionList.IndexOf(s) == m_solutionList.LastIndexOf(s)));
+
+		}	
+
+		public int[] GetPossibleSolutions ()
+		{
+				return m_solutionList.ToArray ();
+		}
+
+		// disables warning about default case,
+		// which we want to keep for safety
+		#pragma warning disable 0162
+		public override string ToString ()
+		{
+        
+				// convert the operator to its string equivalent
+				string operatorString = "";
+				switch (m_operator) {
+				case Operator.PLUS:
+						operatorString = " + ";
+						break;
+				case Operator.MINUS:
+						operatorString = " - ";
+						break;
+				case Operator.TIMES:
+						operatorString = " * ";
+						break;
+				default:
+						throw new System.Exception ("Unexpected operator value: " + m_operator.ToString ());
+				}
+        
+				return m_operandFirst + operatorString + m_operandSecond + " = ?";
+		}
+		#pragma warning restore 0162
+
+		// disables warning about default case,
+		// which we want to keep for safety
+		#pragma warning disable 0162
+		public int GetSolution ()
+		{
+				switch (m_operator) {
+				case Operator.PLUS:
+						return m_operandFirst + m_operandSecond;
+						break;
+				case Operator.MINUS:
+						return m_operandFirst - m_operandSecond;
+						break;
+				case Operator.TIMES:
+						return m_operandFirst * m_operandSecond;
+						break;
+				default:
+						throw new System.Exception ("Unexpected operator value: " + m_operator.ToString ());
+				}
+		}
+		#pragma warning restore 0162
+
+		public int GetSolutionIndex ()
+		{
+				Assert.IsTrue (m_solutionList.IndexOf(GetSolution()) < 4);
+				return m_solutionList.IndexOf (GetSolution ());
+		}
+
+		// disables warning about default case,
+		// which we want to keep for safety
+		#pragma warning disable 0162
+		private int MaxSolution ()
+		{
+				switch (m_operator) {
+				case Operator.PLUS:
+						return MAX_OPERAND + MAX_OPERAND;
+						break;
+				case Operator.MINUS:
+						return MAX_OPERAND;
+						break;
+				case Operator.TIMES:
+						return MAX_OPERAND * MAX_OPERAND;
+						break;
+				default:
+						throw new System.Exception ("Unexpected operator value: " + m_operator.ToString ());
+				}
+		}
+		#pragma warning restore 0162
+}
