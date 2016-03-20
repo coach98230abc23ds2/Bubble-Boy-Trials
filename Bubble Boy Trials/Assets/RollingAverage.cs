bde0536f93f54e933a898734011e8309eaba54a1
Max Dietz <mdietz@wesleyan.edu>
2016-03-20 14:41:32 -0400

    Realized there is a much more efficient way to calculate rolling averages

--- Bubble Boy Trials/Assets/RollingAverage.cs
+++ Bubble Boy Trials/Assets/RollingAverage.cs
@@ -4,53 +4,23 @@ using System.Collections.Generic;
 
 public class RollingAverage
 {
-		// the cached average
-		float? m_average;
-
-		// stores the array of values
-		float[] m_values;
-
-		// points to the oldest value
-		// so it can be replaced next
-		int m_index;
+		int m_count;
+		float m_average;
 
 		public RollingAverage (int count, float defaultValue)
 		{
-				Assert.IsTrue (count > 0);
-				m_values = new float[count];
-				m_index = 0;
-
-				// initialize to a default value
-				// so that initial additions occur slowly
+				m_count = count;
 				m_average = defaultValue;
-				for (int i = 0; i < count; i++) {
-						m_values[i] = defaultValue;
-				}
 		}
 
 		public float GetAverage ()
 		{
-				if (!m_average.HasValue) {
-						m_average = 0;
-						foreach (float value in m_values) {
-								m_average += value;
-						}
-						m_average /= m_values.GetLength(0);
-				}
-
-				return m_average.Value;
+				return m_average;
 
 		}
 
 		public void AddValue (float value)
 		{
-				m_average = null;
-				m_values [m_index] = value;
-				m_index++;
-
-				// reset index if at the end of the list
-				if (m_index >= m_values.GetLength(0)) {
-						m_index = 0;
-				}
+				m_average = (value - m_average) / m_count;
 		}
 }
