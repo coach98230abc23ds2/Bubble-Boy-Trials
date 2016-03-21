6981226226e1a38a36f16f3c8c8de6440b23c80e
victorchu123 <victorchu123@gmail.com>
2016-03-21 11:22:15 -0400

    added Maze Scene

---
+++ Bubble Boy Trials/Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,182 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+using System.Collections.Generic;
+
+public class BattleSystem : MonoBehaviour
+{
+
+		public enum BattleState
+		{
+				player_turn,
+				enemy_turn
+		};
+
+		public Button answer1;
+		public Button answer2;
+		public Button answer3;
+		public Button answer4;
+
+		public Slider time_remaining;
+		public Text battleMessage;
+		public Text problem;
+		public GameObject enemy;
+
+		private GameObject player;
+		private BattleState current_state;
+
+		private static float MAX_TIME_PER_TURN = 10;
+		private static float TIME_INCREASE_ON_WRONG_ANSWER = 1;
+		private static RollingAverage time_per_turn;
+
+		[Range (1, 20)] public static int TimePerTurnAverageLength = 5;
+		[Range (0.25f, 2)] public float ReactionWindow = 1000;
+
+		private float timeRemaining;
+		private float timeToDefend;
+
+		private List<Button> answers;
+
+
+		private int comboChain;
+
+		// Use this for initialization
+		void Start ()
+		{
+				if (time_per_turn == null) {
+						time_per_turn = new RollingAverage(TimePerTurnAverageLength, MAX_TIME_PER_TURN);
+				}
+				player = GameObject.FindGameObjectWithTag ("Player");
+				answers = new List<Button> { answer1, answer2, answer3, answer4 };
+				PlayerTurn ();
+		}
+	
+		// Update is called once per frame
+		void FixedUpdate ()
+		{
+				timeRemaining -= Time.fixedDeltaTime;
+				switch (current_state) {
+				case BattleState.player_turn:
+						time_remaining.value = timeRemaining;
+						if (timeRemaining < 0) {
+								// pretend the player would have gotten it in another second if stuck
+								time_per_turn.AddValue (time_per_turn.GetAverage () + TIME_INCREASE_ON_WRONG_ANSWER);
+								comboChain = 0;
+								EnemyTurn ();
+						}
+						break;
+				case BattleState.enemy_turn:
+						if (timeRemaining < ReactionWindow) {
+								battleMessage.text = "NOW!";
+						} else {
+								battleMessage.text = "WAITING...";
+						}
+						if (timeRemaining < 0) {
+								PlayerTurn ();
+								player.GetComponent<Player>().TakeDamage(10);
+
+						} else if (timeRemaining < ReactionWindow && Input.GetKeyDown (KeyCode.X)) {
+								PlayerTurn ();
+						} else if (Input.GetKeyDown (KeyCode.X)) {
+								PlayerTurn ();
+								player.GetComponent<Player> ().TakeDamage (20);
+						}
+						break;
+				}
+		}
+
+
+		private void ResetButton (Button button)
+		{
+				button.gameObject.SetActive (true);
+				SetListener (button, false);
+		}
+
+
+		/* Sets the button's only listener to be Right/WrongAnswer */
+		private void SetListener (Button button, bool isRightAnswer)
+		{
+				button.onClick.RemoveAllListeners ();
+				if (isRightAnswer) {
+						button.onClick.AddListener (RightAnswer);
+				} else {
+						button.onClick.AddListener (WrongAnswer);
+				}
+		}
+
+		private void ShowPlayerUI ()
+		{
+				answers.ForEach (b => ResetButton (b));
+				problem.gameObject.SetActive (true);
+				time_remaining.gameObject.SetActive (true);
+				battleMessage.gameObject.SetActive (false);
+		}
+
+		private void HidePlayerUI ()
+		{
+				answers.ForEach (b => b.gameObject.SetActive (false));
+				problem.gameObject.SetActive (false);
+				time_remaining.gameObject.SetActive (false);
+				battleMessage.gameObject.SetActive (true);
+		}
+
+
+		private void PlayerTurn ()
+		{
+				timeRemaining = time_per_turn.GetAverage();
+				current_state = BattleState.player_turn;
+
+				// reset buttons and UI elements
+				ShowPlayerUI ();
+
+				// generate new problem
+				Problem newProblem = new Problem ();
+		
+				// set question text fields based on new problem
+				problem.text = newProblem.ToString();
+				problem.gameObject.SetActive (true);
+		
+				// set solution text fields based on new Problem
+				int[] possibleSolutions = newProblem.GetPossibleSolutions ();
+
+				// although this is set as a variable in newProblem
+				// we need to make UI buttons by hand on the scene, so
+				// we should ensure it is equal to 4
+				Assert.AreEqual (possibleSolutions.GetLength (0), answers.Count);
+				for (int i = 0; i < answers.Count; ++i) {
+						answers [i].GetComponentInChildren<Text> ().text = possibleSolutions [i].ToString ();
+				}
+
+				SetListener (answers [newProblem.GetSolutionIndex ()], true);
+		}
+
+		private void RightAnswer ()
+		{
+				time_per_turn.AddValue (time_per_turn.GetAverage() - timeRemaining);
+				int dmg = 10;
+				if (comboChain >= 3) {
+						dmg += 10 * comboChain - 2;
+				}
+
+				enemy.GetComponent<Player> ().TakeDamage (dmg);
+
+				comboChain++;
+
+				HidePlayerUI ();
+				EnemyTurn ();
+		}
+
+		private void WrongAnswer ()
+		{
+				time_per_turn.AddValue (time_per_turn.GetAverage () + TIME_INCREASE_ON_WRONG_ANSWER);
+				comboChain = 0;
+				HidePlayerUI ();
+				EnemyTurn ();
+		}
+
+		private void EnemyTurn ()
+		{
+				current_state = BattleState.enemy_turn;
+				timeRemaining = Random.Range (2, 2 + time_per_turn.GetAverage());
+		}
+}
---
+++ Bubble Boy Trials/Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+		public int Health = 30;
+		public Sprite IdleSprite;
+		public Sprite HurtSprite;
+		public Sprite DeadSprite;
+
+		private float animCycle;
+		private GameObject healthBar;
+		private bool isDead = false;
+    
+		// Use this for initialization
+		void Start ()
+		{
+				healthBar = GameObject.Instantiate (Resources.Load("HealthBar"), new Vector3 (0, 0, 0), Quaternion.identity) as GameObject;
+				healthBar.transform.SetParent (GameObject.Find ("Canvas").GetComponent<RectTransform> (), false);
+				healthBar.GetComponent<Slider> ().value = Health;
+				GetComponent<SpriteRenderer> ().sprite = IdleSprite;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				RectTransform CanvasRect = GameObject.Find ("Canvas").GetComponent<RectTransform> ();
+				Vector2 ViewportPos = Camera.main.WorldToViewportPoint (transform.position);
+
+				Vector2 ScreenPos = new Vector2 (
+						(ViewportPos.x * CanvasRect.sizeDelta.x) - (CanvasRect.sizeDelta.x * 0.5f),
+						(ViewportPos.y * CanvasRect.sizeDelta.y) - (CanvasRect.sizeDelta.y * 0.5f)
+				);
+
+				healthBar.GetComponent<RectTransform> ().anchoredPosition = ScreenPos + new Vector2 (0, 50);
+
+				// run through animations
+				animCycle -= Time.deltaTime;
+				if (isDead) {
+						GetComponent<SpriteRenderer> ().sprite = DeadSprite;
+				} else if (animCycle < 0) {
+						animCycle = 0.5f;
+						if (GetComponent<SpriteRenderer> ().sprite == HurtSprite)
+								GetComponent<SpriteRenderer> ().sprite = IdleSprite;
+						else
+								GetComponent<SpriteRenderer> ().sprite = HurtSprite;
+				}
+		}
+
+		public void TakeDamage (int damage)
+		{
+				Health -= damage;
+
+				if (healthBar != null) {
+						healthBar.GetComponent<Slider> ().value = Health;
+				}
+
+				if (Health <= 0) {
+					isDead = true;
+				}
+		}
+}
