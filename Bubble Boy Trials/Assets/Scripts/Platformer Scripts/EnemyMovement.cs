fc5ef503638a1900712f8cbddd9fcd3390d5df95
victorchu123 <victorchu123@gmail.com>
2016-04-01 12:39:56 -0400

    reorganized scripts, added more intelligent enemy spawning

---
+++ Bubble Boy Trials/Assets/Scripts/Platformer Scripts/EnemyMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+  public int m_speed2 = 5;
+  private float m_timer = 0.0f;
+	
+	void FixedUpdate()
+  {	
+  	GetComponent<Rigidbody2D>().AddForce(Vector3.left * m_speed2);
+   //  // moves the game object 
+   //  if (m_timer > 5.0f){
+			// GetComponent<Rigidbody2D>().AddForce(Vector3.left * m_speed2);
+   //  }
+  }
+}
