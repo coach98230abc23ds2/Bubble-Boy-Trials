44287affaf485592d6b07de8d5ecf18161de0ea3
Donovan Brady <dbrady@wesleyan.edu>
2016-04-01 12:25:49 -0400

    Adding shooting mechanism..no projectile yet

--- Bubble Boy Trials/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Bubble Boy Trials/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -35,6 +35,7 @@ namespace UnityStandardAssets._2D
             // Pass all parameters to the character control script.
             m_Character.Move(h, crouch, m_Jump);
             m_Jump = false;
+
         }
     }
 }
