c7aded3eafa7d01dca70a7c900331272849e560d
victorchu123 <victorchu123@gmail.com>
2016-04-01 22:35:31 -0400

    enemy spawner is fully functional; enemies will only appear when the player is in the vicinity (30 x units away) and will uniquely spawn. Moreover, the enemies will despawn as soon as they fall off the edge

--- Bubble Boy Trials/Assets/Scripts/Platformer Scripts/PlatformEnemy.cs
+++ Bubble Boy Trials/Assets/Scripts/Platformer Scripts/PlatformEnemy.cs
@@ -3,19 +3,18 @@ using System.Collections;
 
 public class PlatformEnemy : MonoBehaviour {
 	
-    public EnemySpawner spawner; 
+    private EnemySpawner spawner; 
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start () {
+        spawner = Camera.main.GetComponent<EnemySpawner>();
+    }
+
+    // Update is called once per frame
+    void Update () {
         float y_pos = this.transform.position.y;
         if (y_pos <= -10){
             spawner.RemoveFromDict(this.name, y_pos); 
         }
-
-	}
+    }
 }
