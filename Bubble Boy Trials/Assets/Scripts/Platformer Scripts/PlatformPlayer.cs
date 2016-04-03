a21b352446e40d75eeb9bd472e30acb0516d28d2
victorchu123 <victorchu123@gmail.com>
2016-04-03 18:12:27 -0400

    added enemy death interaction

---
+++ Bubble Boy Trials/Assets/Scripts/Platformer Scripts/PlatformPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformPlayer : MonoBehaviour {
+
+    private int m_lives = 3; //player's remaining lives
+    private int m_score = 0; //player's current score
+
+    //increases score by the increment number
+    public void GainScore (int increment){
+        m_score += increment;
+    }
+
+    //decreases lives by 1
+    public void LoseLives (){
+        m_lives--; 
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
