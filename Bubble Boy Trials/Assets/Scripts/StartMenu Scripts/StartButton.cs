9a4a4beeb8cb047b4fa746595aeb10cc2371e45b
victorchu123 <victorchu123@gmail.com>
2016-05-03 11:42:51 -0400

    fixed restart issue where previous cutscene was used

--- Bubble Boy Trials/Assets/Scripts/StartMenu Scripts/StartButton.cs
+++ Bubble Boy Trials/Assets/Scripts/StartMenu Scripts/StartButton.cs
@@ -10,18 +10,21 @@ public class StartButton : MonoBehaviour {
     private GameObject canvas;
     private GameObject camera;
     private GameObject event_system;
+    private GameObject char_choice;
 
     void Awake()
     {   
         canvas = GameObject.Find("Canvas");
         camera = GameObject.Find("Main Camera");
         event_system = GameObject.Find("EventSystem");
+        char_choice = GameObject.Find("CharacterChoice");
     }
 
     public void StartCharacterSelection ()
     {
         SceneManager.LoadScene("CharacterSelection", LoadSceneMode.Additive);
         Scene scene = SceneManager.GetSceneByName("CharacterSelection");
+        Destroy(char_choice);
         SceneManager.MoveGameObjectToScene(event_system, scene);
         Destroy(camera);
         SceneManager.SetActiveScene(scene);
