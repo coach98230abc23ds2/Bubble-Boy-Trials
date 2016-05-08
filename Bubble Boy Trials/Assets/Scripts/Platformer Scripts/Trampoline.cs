0add9ade1b0f318cbb53fc534809309b3f69f4af
victorchu123 <victorchu123@gmail.com>
2016-05-07 22:35:02 -0400

    improved trampoline a bit

--- Bubble Boy Trials/Assets/Scripts/Platformer Scripts/Trampoline.cs
+++ Bubble Boy Trials/Assets/Scripts/Platformer Scripts/Trampoline.cs
@@ -5,8 +5,7 @@ using UnityStandardAssets._2D;
 public class Trampoline : MonoBehaviour {
 
     private bool m_touched = false;
-    private bool m_is_rested = true;
-    private bool m_is_accelerating = false;
+    private bool m_is_falling = true;
     private float jump_force;
     private float player_y_velocity;
     private Rigidbody2D player_rg2d;
@@ -15,65 +14,51 @@ public class Trampoline : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         player = GameObject.Find("Player");
-	    jump_force = player.GetComponent<PlatformerCharacter2D>().m_JumpForce; 
+	    jump_force = player.GetComponent<PlatformerCharacter2D>().m_JumpForce/10; 
         player_rg2d = player.GetComponent<Rigidbody2D>();
-        player_y_velocity = player_rg2d.velocity.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-	    if (player_y_velocity > 0f) 
+        player_y_velocity = player_rg2d.velocity.y;
+        Debug.Log(System.Convert.ToString(player_y_velocity));
+
+        if (player_y_velocity < 0f)
         {
-            m_is_rested = false;
-            m_is_accelerating = true;
-        }
-        else if (player_y_velocity < 0f)
-        {
-            m_is_accelerating = false;
-            m_is_rested = false;
+            m_is_falling = true;
         }
         else
         {
-            m_is_rested = true;
-            m_is_accelerating = false;
+            m_is_falling = false;
         }
 
-        if (player_rg2d.position.y > 50.2f)
+        if (player_rg2d.position.y > 54.4f)
         {
 //            player_rg2d.velocity.Normalize();
-            player_rg2d.velocity = Vector3.zero;
+            float temp_y_velocity = -1f;
+            player_rg2d.velocity = new Vector2(0, temp_y_velocity);
         }
 	}
-
-    void OnCollisionExit2D(Collision2D coll)
-    {
-        if (coll.gameObject.tag == "Player")
-        {   
-           coll.gameObject.GetComponent<AudioSource>().Play();
-//           m_touched = false;
-        }
-    }
+    
 
     void OnCollisionStay2D(Collision2D coll)
     {
         if (coll.gameObject.tag == "Player")
         {   
 
-            if (m_is_rested)
+            if (m_is_falling)
             {
-                player_rg2d.AddForce(new Vector2(0, .4f * jump_force));
-            }
-            if (m_is_accelerating)
-            {
-                player_rg2d.AddForce(new Vector2(0, .6f * jump_force));
-            }
-            else
-            {
-                player_rg2d.AddForce(new Vector2(0, .5f * jump_force));
+                this.gameObject.GetComponent<AudioSource>().Play();
+                player_rg2d.AddForce(new Vector2(0, .2f* jump_force * Mathf.Abs(player_y_velocity)));
             }
 
-            this.gameObject.GetComponent<AudioSource>().Play();
+//            if (player_y_velocity < 0f)
+//            {   
+//                float applied_force = (Mathf.Abs(player_y_velocity) + jump_force) * multiplier;
+//                player_rg2d.AddForce(new Vector2(0, )); 
+//            }
+
 //            m_touched = true;
         }
     }
